28810a73fceae21bb56fffd63f86fdf6f425a444
evvlin <illarionova.ev@phystech.edu>
2026-09-22 18:00:52 +0300

    Add files via upload
    онегин с неработающей сортировкой с конца))

--- onegin.cpp
+++ onegin.cpp
@@ -1,65 +1,86 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
-size_t ReadFromFile(const char *name, char *index[], const int a);
-
-
+size_t ReadFromFile(char *text, char **index, const size_t size_of_file, const int number_of_strings);
 int CompareString(const void *a, const void *b);
 int strlen( const char *str);
 int strcmp(const char* str1, const char *str2);
 char *strcpy(const char *start, char *destination);
 char *strdup(const char *str);
 void ChangeValues(void **value1, void **value2);
-void Sort(char* index[], int n, int (*CompareFunc)(const void *a, const void *b));
+void Sort_by_the_start(char* index[], int n, int (*CompareFunc)(const void *a, const void *b));
+void Sort_by_the_end(char* index[], int n, int (*CompareFunc)(const void *a, const void *b));
 
 const int MAX_LEN = 100;
 
-
-
-int main()
+int main() 
 {
-  const int number = 12;
-  char *index[number]= {};
-  printf("1\n");
-  ReadFromFile("onegin.txt", index, number);
-    
-  Sort(index, number, &CompareString);
+    FILE *file = fopen("onegin.txt", "r");
+    if (file == NULL) {
+        printf("file is govno\n");
+        return 1;
+    }
 
-  for(int i = 0; i < number; i++)
-  {
-    printf("%s ", index[i]);
-  }
-  return 0;
+    struct stat file_status;
+    stat("onegin.txt", &file_status);
+    const size_t size_of_file = file_status.st_size;
+
+    char *text = (char *)calloc(size_of_file + 1, sizeof(char));
+    int n = 0;
+    int j = 0;
+    char ch;
+
+    while (j < size_of_file && (ch = fgetc(file)) != EOF) 
+    {
+        text[j] = ch;
+        j++;
+    }
+
+    for (int i = 0; i < size_of_file; i++) 
+    {
+        if (text[i] == '\n' || text[i] == '\r') 
+        {
+            text[i] = '\0';
+            n++;
+        }
+    }
+    const int number_of_strings = n;
+
+    char **index = (char **)calloc(number_of_strings + 1, sizeof(char *));
+
+    
+    ReadFromFile(text, index, size_of_file, number_of_strings);
+    Sort_by_the_end(index, number_of_strings, &CompareString);
+
+    for (int i = 0; i < number_of_strings; i++) 
+    { 
+        printf("%s\n", index[i]);
+    }
+
+    
+    return 0;
 }
 
 
-size_t ReadFromFile(const char *name, char *index[], const int a)
+size_t ReadFromFile(char *text, char **index, const size_t size_of_file, const int number_of_strings) 
 {
-  FILE* file = fopen(name, "r");
-  if(file == NULL)
-  {
-    printf("file is govno\n");
-    return 1; 
-  }
+    char *cursor = text; 
+    int j = 0;
+    int l = 1;
 
-  char buffer[MAX_LEN]; 
-  int i = 0;
-  
-  while(i < a)
-  {
-    if (fgets(buffer, MAX_LEN, file) == NULL)
+    index[0] = text;
+    while (j < size_of_file && l < number_of_strings) 
     {
-      break;
-    } 
-  
-    index[i] = strdup(buffer); 
-    
-    i++;
-  }
-  fclose(file);
-
-  return 0;
+        if (text[j] == '\0') 
+        {
+            cursor = &text[j + 1];
+            index[l++] = cursor;
+        }
+        j++;
+    }
+    return 0;
 }
 
 
@@ -67,7 +88,7 @@ int strlen( const char *str)
 {
   int length = 0;
 
-  while ( str[length] != '\0')
+  while(str[length] != '\0')
   {
     length++;
   }
@@ -102,8 +123,8 @@ int strcmp(const char* str1, const char *str2)
             return (unsigned char)char1 - (unsigned char)char2;
         }
 
-        str1++;
-        str2++;
+        if(*str1 != '\0') str1++;
+        if(*str2 != '\0') str2++;
     }
     
     return (unsigned char)tolower((unsigned char)*str1) - (unsigned char)tolower((unsigned char)*str2);
@@ -152,7 +173,7 @@ void ChangeValues(void **value1, void **value2)
     *value1 = temp;
 }
 
-void Sort(char* index[], int n, int (*CompareFunc)(const void *a, const void *b))
+void Sort_by_the_start(char* index[], int n, int (*CompareFunc)(const void *a, const void *b))
 {
     for (int j = 0; j < n; j++)
     {
@@ -161,6 +182,25 @@ void Sort(char* index[], int n, int (*CompareFunc)(const void *a, const void *b)
             if ((*CompareFunc)(&index[i], &index[i + 1]) > 0)
             {
                 ChangeValues((void **)&index[i], (void **)&index[i + 1]);
+            }
+        }
+    }
+}
+
+void Sort_by_the_end(char* index[], int n, int (*CompareFunc)(const void *a, const void *b))
+{
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (strlen(index[i]) > 0 && strlen(index[i + 1]) > 0)
+            {
+                char *ptr1 = index[i] + strlen(index[i]) - 1;
+                char *ptr2 = index[i + 1] + strlen(index[i + 1]) - 1;
+                if ((*CompareFunc)(&ptr1, &ptr2) > 0)
+                {
+                    ChangeValues((void **)&index[i], (void **)&index[i + 1]);
+                }
             }
         }
     }
