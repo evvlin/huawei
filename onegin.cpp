4aeb18c23f23247da7592d0fa716d55d843b7382
evvlin <illarionova.ev@phystech.edu>
2026-09-23 01:26:46 +0300

    Add files via upload
    доделанный онегин

--- onegin.cpp
+++ onegin.cpp
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 
-size_t ReadFromFile(char *text, char **index, const size_t size_of_file, const int number_of_strings);
-int CompareString(const void *a, const void *b);
-int strlen( const char *str);
+size_t Massiv_filling(char *text, char **index, const size_t size_of_file, const int number_of_strings);
+int Compare_string_by_the_start(const void *a, const void *b);
+int Compare_string_by_the_end(const void *a, const void *b);
 int strcmp(const char* str1, const char *str2);
 char *strcpy(const char *start, char *destination);
 char *strdup(const char *str);
 void ChangeValues(void **value1, void **value2);
 void Sort_by_the_start(char* index[], int n, int (*CompareFunc)(const void *a, const void *b));
 void Sort_by_the_end(char* index[], int n, int (*CompareFunc)(const void *a, const void *b));
-
-const int MAX_LEN = 100;
+int OutPutSort(char **index, char **index_sort_by_the_start, char **index_sort_by_the_end, const int number_of_strings);
 
 int main() 
 {
@@ -28,43 +28,71 @@ int main()
     const size_t size_of_file = file_status.st_size;
 
     char *text = (char *)calloc(size_of_file + 1, sizeof(char));
-    int n = 0;
-    int j = 0;
-    char ch;
+    fread(text, sizeof(char), size_of_file, file);
 
-    while (j < size_of_file && (ch = fgetc(file)) != EOF) 
-    {
-        text[j] = ch;
-        j++;
-    }
-
+    int schitaem_stroki = 0;
     for (int i = 0; i < size_of_file; i++) 
     {
         if (text[i] == '\n' || text[i] == '\r') 
         {
             text[i] = '\0';
-            n++;
+            schitaem_stroki++;
         }
     }
-    const int number_of_strings = n;
 
+    const int number_of_strings = schitaem_stroki;
     char **index = (char **)calloc(number_of_strings + 1, sizeof(char *));
+    char **index_sort_by_the_end = (char **)calloc(number_of_strings + 1, sizeof(char *));
+    char **index_sort_by_the_start = (char **)calloc(number_of_strings + 1, sizeof(char *));
 
+    Massiv_filling(text, index, size_of_file, number_of_strings);
+    memcpy(index_sort_by_the_end, index, (number_of_strings + 1) * sizeof(char *));
+    memcpy(index_sort_by_the_start, index, (number_of_strings + 1) * sizeof(char *));
+
+    OutPutSort(index, index_sort_by_the_start, index_sort_by_the_end, number_of_strings);
     
-    ReadFromFile(text, index, size_of_file, number_of_strings);
-    Sort_by_the_end(index, number_of_strings, &CompareString);
-
-    for (int i = 0; i < number_of_strings; i++) 
-    { 
-        printf("%s\n", index[i]);
-    }
-
-    
+    free(text);
+    fclose(file);
     return 0;
 }
 
 
-size_t ReadFromFile(char *text, char **index, const size_t size_of_file, const int number_of_strings) 
+int OutPutSort(char **index, char **index_sort_by_the_start, char **index_sort_by_the_end, const int number_of_strings)
+{
+    FILE *fp = fopen("onegin_sorted.txt", "w");
+    Sort_by_the_start(index_sort_by_the_start, number_of_strings, &Compare_string_by_the_start);
+
+    for (int k = 0; k < number_of_strings; k++) 
+    { 
+        if(strlen(index_sort_by_the_start[k]) > 0)
+        {
+            fprintf(fp, "%s\n", index_sort_by_the_start[k]);
+        }
+    }
+
+    Sort_by_the_end(index_sort_by_the_end, number_of_strings, &Compare_string_by_the_end);
+
+    for (int i = 0; i < number_of_strings; i++) 
+    { 
+        if(strlen(index_sort_by_the_start[i]) > 0)
+        {
+            fprintf(fp, "%s\n", index_sort_by_the_end[i]);
+        }
+    }
+
+    for (int p = 0; p < number_of_strings; p++) 
+    { 
+        if(strlen(index[p]) > 0)
+        {
+            fprintf(fp, "%s\n", index[p]);
+        }
+    }
+
+    fclose(fp);
+
+}
+
+size_t Massiv_filling(char *text, char **index, const size_t size_of_file, const int number_of_strings) 
 {
     char *cursor = text; 
     int j = 0;
@@ -83,21 +111,23 @@ size_t ReadFromFile(char *text, char **index, const size_t size_of_file, const i
     return 0;
 }
 
+// int strlen(const char *str)
+// {
+//   int length = 0;
 
-int strlen( const char *str)
+//   while(str[length] != '\0')
+//   {
+//     length++;
+//   }
+
+//   return length;
+// }
+
+int Compare_string_by_the_start(const void *a, const void *b)         
 {
-  int length = 0;
+    char *str1 = *(char **) a;
+    char *str2 = *(char **) b;
 
-  while(str[length] != '\0')
-  {
-    length++;
-  }
-
-  return length;
-}
-
-int strcmp(const char* str1, const char *str2)
-{
     while (*str1 != '\0' || *str2 != '\0')
     {
         while (*str1 != '\0' && !isalpha((unsigned char)*str1))
@@ -126,44 +156,43 @@ int strcmp(const char* str1, const char *str2)
         if(*str1 != '\0') str1++;
         if(*str2 != '\0') str2++;
     }
-    
-    return (unsigned char)tolower((unsigned char)*str1) - (unsigned char)tolower((unsigned char)*str2);
+   return (unsigned char)tolower((unsigned char)*str1) - (unsigned char)tolower((unsigned char)*str2); 
 }
 
-
-char *strcpy(const char *start, char *destination)
+int Compare_string_by_the_end(const void *a, const void *b)         
 {
-  char *link = destination;
-  while(*start != '\0')
-  {
-     *destination = *start;
-     destination++;
-     start++;
-  }
+    char *str1 = *(char **) a;
+    char *str2 = *(char **) b;
 
- *destination = '\0';
- return link;
-}
+    while (*str1 != '\0' || *str2 != '\0')
+    {
+        while (*str1 != '\0' && !isalpha((unsigned char)*str1))
+        {
+            str1--;
+        }
+        
+        while (*str2 != '\0' && !isalpha((unsigned char)*str2))
+        {
+            str2--;
+        }
 
-char *strdup(const char *str)
-{
-    
-    size_t len = strlen(str);
-    char *line = (char *)calloc(len + 1, sizeof(char)); 
-    
-    strcpy(str, line); 
-    
-    return line;
-}
+        if (*str1 == '\0' && *str2 == '\0')
+        {
+            break;
+        }
 
+        char char1 = tolower((unsigned char)*str1);
+        char char2 = tolower((unsigned char)*str2);
 
-int CompareString(const void *a, const void *b)         
-{
-    char *value_a = *(char **) a;
-    char *value_b = *(char **) b;
+        if (char1 != char2)
+        {
+            return (unsigned char)char1 - (unsigned char)char2;
+        }
 
-    return strcmp(value_a, value_b);
-
+        if(*str1 != '\0') str1--;
+        if(*str2 != '\0') str2--;
+    }
+   return (unsigned char)tolower((unsigned char)*str1) - (unsigned char)tolower((unsigned char)*str2); 
 }
 
 void ChangeValues(void **value1, void **value2) 
@@ -193,15 +222,13 @@ void Sort_by_the_end(char* index[], int n, int (*CompareFunc)(const void *a, con
     {
         for (int i = 0; i < n - 1; i++)
         {
-            if (strlen(index[i]) > 0 && strlen(index[i + 1]) > 0)
+            char *ptr1 = index[i] + strlen(index[i]) - 1;
+            char *ptr2 = index[i + 1] + strlen(index[i + 1]) - 1;
+            if ((*CompareFunc)(&ptr1, &ptr2) > 0)
             {
-                char *ptr1 = index[i] + strlen(index[i]) - 1;
-                char *ptr2 = index[i + 1] + strlen(index[i + 1]) - 1;
-                if ((*CompareFunc)(&ptr1, &ptr2) > 0)
-                {
-                    ChangeValues((void **)&index[i], (void **)&index[i + 1]);
-                }
+                ChangeValues((void **)&index[i], (void **)&index[i + 1]);
             }
+
         }
     }
 }
